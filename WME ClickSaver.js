1039eeae8cd8d9c6dbead98ddbaf7b0bb93b9770
mapomatic <mapomatic.waze@gmail.com>
2017-10-26 23:05:03 -0400

    change script name (remove beta)

--- WME ClickSaver.js
+++ WME ClickSaver.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         WME ClickSaver (beta)
+// @name         WME ClickSaver
 // @namespace    https://greasyfork.org/users/45389
 // @version      0.8.6
 // @description  Various UI changes to make editing faster and easier.
