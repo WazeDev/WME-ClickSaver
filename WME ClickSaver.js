5154122e860359eebf6ca8f2155724850a740532
mapomatic <mapomatic.waze@gmail.com>
2018-04-28 11:48:58 -0400

    Remove (beta) from name after merge

--- WME ClickSaver.js
+++ WME ClickSaver.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         WME ClickSaver (beta)
+// @name         WME ClickSaver
 // @namespace    https://greasyfork.org/users/45389
 // @version      2018.04.28.001
 // @description  Various UI changes to make editing faster and easier.
