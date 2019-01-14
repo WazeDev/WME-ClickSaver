6281c8ba981e8792af146bb73c06725224bcbc04
MapOMatic <mapomatic.waze@gmail.com>
2019-01-13 20:12:27 -0500

    version # bump

--- WME ClickSaver.js
+++ WME ClickSaver.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            WME ClickSaver (beta)
 // @namespace       https://greasyfork.org/users/45389
-// @version         2018.12.21.001
+// @version         2019.01.13.001
 // @description     Various UI changes to make editing faster and easier.
 // @author          MapOMatic
 // @include         /^https:\/\/(www|beta)\.waze\.com\/(?!user\/)(.{2,6}\/)?editor\/?.*$/
