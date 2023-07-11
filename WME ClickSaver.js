7828ebecac8200bc84afe3bdff84d0396d10817b
Jason Whipkey <60714856+fuji2086@users.noreply.github.com>
2023-07-10 21:56:16 -0400

    Update WME ClickSaver.js
    Updated onAddAltCityButtonClick() function to handle street names that have not been saved yet.

--- WME ClickSaver.js
+++ WME ClickSaver.js
@@ -370,9 +370,7 @@
             const elem = await waitForElem('wz-autocomplete.alt-street-name');
             elem.focus();
             let result = await waitForShadowElem('wz-autocomplete.alt-street-name', ['wz-text-input']);
-            result.shadowElem.click();
-            result = await waitForShadowElem('wz-autocomplete.alt-street-name', [`wz-menu-item[item-id="${streetID}"]`]);
-            result.shadowElem.click();
+            result.shadowElem.value = W.model.streets.objects[streetID].name;
             result = await waitForShadowElem('wz-autocomplete.alt-city-name', ['wz-text-input']);
             result.shadowElem.value = null;
             result.parentElem.focus();
